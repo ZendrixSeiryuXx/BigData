c9f707ebab055925ba12f592fc24ee72ea31d958
JuanEudes <jem.lopez.19@gmail.com>
2024-06-08 12:43:48 -0500

    Avances Juan

--- app/src/main/java/com/bigdatacorp/bigdata/Login/LoginActivity.kt
+++ app/src/main/java/com/bigdatacorp/bigdata/Login/LoginActivity.kt
@@ -1,4 +1,12 @@
 package com.bigdatacorp.bigdata.Login
 
-class LoginActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bigdatacorp.bigdata.R
+
+class LoginActivity: AppCompatActivity()  {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
 }
