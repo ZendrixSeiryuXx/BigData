441e780606329967270bdec8e1da37bd4a2b8f76
Zendrix Seiryu <zendrix.seiryu.devx@gmail.com>
2024-06-08 17:16:00 -0500

    a

--- app/src/main/java/com/bigdatacorp/bigdata/Favoritos/FavoritosActivity.kt
+++ app/src/main/java/com/bigdatacorp/bigdata/Favoritos/FavoritosActivity.kt
@@ -1,4 +1,24 @@
 package com.bigdatacorp.bigdata.Favoritos
 
-class FavoritosActivity {
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.bigdatacorp.bigdata.R
+
+class FavoritosActivity: AppCompatActivity()   {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_favoritos)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_favoritos)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+
 }
